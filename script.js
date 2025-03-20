ec17f272c9c4da70ce6dfebf1ef35575348fdc05
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-20 14:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let element = document.querySelector("#val");
+let mybtn = document.querySelector("#btn");
+
+mybtn.addEventListener("click",function (){
+	
+	alert(element.textContent);
+	let eleval = Number(element.textContent);
+	eleval += 1;
+	element.textContent = eleval;
+	
+});
