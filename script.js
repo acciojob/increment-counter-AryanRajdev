1459238493fb02111d9f12e9fd8fd48c98487128
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-20 14:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let mybtn = document.querySelector("#incrementBtn");
 
 mybtn.addEventListener("click",function (){
 	
-	alert(element.textContent);
+	alert(Number(element.textContent));
 	let eleval = Number(element.textContent);
 	eleval += 1;
 	element.textContent = eleval;
